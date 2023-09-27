5070997c11382ff8bb0c19f6c25b96496b500860
Brad Griffith <brad@buckeyeinteractive.com>
2023-09-27 15:35:53 -0400

    Always open a new database connection to prevent trying a closed connection

--- src/lib/db/mysql.js
+++ src/lib/db/mysql.js
@@ -5,14 +5,13 @@ import mysql from "mysql2/promise";
 let mysqlconn = null;
 
 export function mysqlconnFn() {
-  if (!mysqlconn) {
-    mysqlconn = mysql.createConnection({
-      host: DB_HOST,
-      user: DB_USER,
-      password: DB_PASSWORD,
-      database: DB_NAME,
-    });
-  }
+  //always create a new connection to avoid closed connections
+  mysqlconn = mysql.createConnection({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+  });
 
   return mysqlconn;
 }
